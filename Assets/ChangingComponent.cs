84efbcef2011fa5db7b991636721c3af453060f3
bvc_lvthn <ben.vancitters@lvthn.com>
2018-12-05 12:25:41 -0600

    initial commit

---
+++ Assets/ChangingComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangingComponent : MonoBehaviour {
+
+    public float FloatVar = 0.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FloatVar = Mathf.Sin(Time.fixedTime);
+	}
+}
